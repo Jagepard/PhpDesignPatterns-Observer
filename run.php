4ac7d2490c30ecd5e9b0d4c267f9779b0825422a
Jagepard <dankorot@gmail.com>
2017-09-01 18:26:09 +0300

    Добавляет detachObserver

--- run.php
+++ run.php
@@ -14,3 +14,8 @@ $team->attachObserver(new FootballObserver('Вася'));
 $team->teamAction(new FootballEvent(FootballEvent::GOAL, $team));
 $team->teamAction(new FootballEvent(FootballEvent::MISS, $team));
 $team->teamAction(new FootballEvent(FootballEvent::FIRE, $team));
+
+$team->detachObserver('Вася');
+$team->attachObserver(new FootballObserver('Иван'));
+
+$team->teamAction(new FootballEvent(FootballEvent::GOAL, $team));
