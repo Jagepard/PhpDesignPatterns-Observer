cf85a94952bb6d24d594702cdef5b782eddadd16
Jagepard <dankorot@gmail.com>
2017-09-01 17:52:02 +0300

    Обновляет

---
+++ src/EventInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * @author    : Korotkov Danila <dankorot@gmail.com>
+ * @copyright Copyright (c) 2017, Korotkov Danila
+ * @license   http://www.gnu.org/licenses/gpl.html GNU GPLv3.0
+ */
+
+namespace Behavioral\Observer;
+
+/**
+ * Interface EventInterface
+ *
+ * @package Behavioral\Observer
+ */
+interface EventInterface
+{
+
+    /**
+     * @return string
+     */
+    public function getEventName(): string ;
+
+    /**
+     * @return FootballSubject
+     */
+    public function getFootballSubject(): FootballSubject;
+}
