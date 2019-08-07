1d7f0073230ebeeb4d848caf956a1403c365fac5
Jagepard <dankorot@gmail.com>
2019-08-07 16:50:15 +0300

    Update

--- src/FootballEvent.php
+++ src/FootballEvent.php
@@ -9,10 +9,6 @@ declare(strict_types=1);
 
 namespace Behavioral\Observer;
 
-/**
- * Class FootballEvent
- * @package Behavioral\Observer
- */
 class FootballEvent implements EventInterface
 {
     const GOAL = 'Goal!!!';
